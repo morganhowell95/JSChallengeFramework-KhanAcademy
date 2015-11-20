884b796fdc3ca28abe78e7f213b5bbc2bd1d5668
morganhowell95 <morgan.howell95@gmail.com>
2015-11-19 23:25:49 -0500

    adding password feature

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,24 @@
 class StaticPagesController < ApplicationController
+
   def home
+  	if cookies[:logged_in] != "cookie_set: 1234567890!@"
+  		render :nothing => true, :status => 400
+  	end 
   end
+
+  def set_auth
+  	pass = params[:password]
+  	if pass == "KhanAcademyRocks!"
+  		cookies[:logged_in] = "cookie_set: 1234567890!@"
+  		render 'home'
+  	else
+  		flash.now[:danger] = 'Invalid Password'
+  		render 'access'
+  	end
+  end
+
+  def access
+
+  end
+
 end
